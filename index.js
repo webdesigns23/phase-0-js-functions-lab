e34077158c03891caf21bae6b1274d212875d05b
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-05-09 14:28:17 -0600

    fixed line 51 from being commented out for test to work

--- index.js
+++ index.js
@@ -48,4 +48,4 @@ function calculateDiscountedPrice (originalPrice, discountPercentage) {
 console.log(calculateDiscountedPrice(100,150));
 
 // This is required for the test to function properly  
-//module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
